21cac4569925a439f3f573597bad17c6c57e112c
Ondřej Vodička <ondrej.vodicka@kolibrik.net>
2024-07-18 00:01:40 +0200

    feat: add added timestamp to model and create migrations

--- library/models.py
+++ library/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 
@@ -5,6 +7,10 @@ class Author(models.Model):
     first_name = models.CharField(max_length=70)
     last_name = models.CharField(max_length=70)
     birth_date = models.DateField()
+    add_date = models.DateTimeField("date added", auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
 
 class Book(models.Model):
@@ -12,3 +18,7 @@ class Book(models.Model):
     isbn = models.CharField(max_length=17)  # 17 chars = ISBN13 with hyphens
     pub_date = models.DateField("date published")
     authors = models.ManyToManyField(Author)
+    add_date = models.DateTimeField("date added", auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.title
