7bce02e61119eac583a6ab03732c16603dda43a2
Ondřej Vodička <ondrej.vodicka@kolibrik.net>
2024-07-18 00:03:17 +0200

    feat: add basic endpoints for models

--- library/views.py
+++ library/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.request import Request
+from rest_framework.response import Response
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
+class AuthorViewSet(ModelViewSet):
+    queryset = Author.objects.all().order_by("-add_date")
+    serializer_class = AuthorSerializer
 
-def index(request):
-    return HttpResponse("Hello, world.")
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all().order_by("-add_date")
+    serializer_class = BookSerializer
