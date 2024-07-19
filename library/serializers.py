b6cd3b33358aaf109d04f80fa9e22ca6417afb67
Ondřej Vodička <ondrej.vodicka@kolibrik.net>
2024-07-19 10:16:37 +0200

    fix: rename the past datetime validator, create tests for fetching data from the server

--- library/serializers.py
+++ library/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 from .models import Author, Book
-from .validators import PastDateTime
+from .validators import PastDateTimeValidator
 
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
-    birth_date = serializers.DateField(validators=[PastDateTime()])
+    birth_date = serializers.DateField(validators=[PastDateTimeValidator()])
     class Meta:
         model = Author
         fields = ["id", "url", "first_name", "last_name", "birth_date"]
 
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
-    pub_date = serializers.DateField(validators=[PastDateTime()])
+    pub_date = serializers.DateField(validators=[PastDateTimeValidator()])
     class Meta:
         model = Book
         fields = ["id", "url", "title", "isbn", "pub_date", "authors"]
--- library/validators.py
+++ library/validators.py
@@ -1,17 +1,13 @@
 import datetime
-import pytz
 from rest_framework.serializers import ValidationError
 
-class PastDateTime:
+class PastDateTimeValidator:
     def __init__(self) -> None:
         pass
     
     def __call__(self, timestamp) -> None:
-        # TODO: This is probably wrong... Look at the docs
         if isinstance(timestamp, datetime.date):
             timestamp = datetime.datetime.combine(timestamp, datetime.time())
-            # tz = pytz.timezone("Europe/Prague")
-            # timestamp = tz.localize(timestamp)
 
         if(timestamp > datetime.datetime.now()):
             raise ValidationError("The datetime must be in the past.")
