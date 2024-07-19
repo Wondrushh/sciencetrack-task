fdc7d73a78129a4a5164e90df82cc601271eeb4d
Wondrushh <ondrej.vodicka32@gmail.com>
2024-07-19 11:18:19 +0200

    feat: implement isbn validator and remove hyphens before adding new book to the model

--- library/serializers.py
+++ library/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Author, Book
-from .validators import PastDateTimeValidator
+from .validators import PastDateTimeValidator, ISBNValidator
 
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
@@ -11,7 +11,15 @@ class AuthorSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
+    isbn = serializers.CharField(validators=[ISBNValidator()])
     pub_date = serializers.DateField(validators=[PastDateTimeValidator()])
+    
+    def create(self, validated_data):
+        # Remove hyphens from the ISBN
+        validated_data["isbn"] = validated_data["isbn"].replace("-", "")
+
+        return super().create(validated_data)
+
     class Meta:
         model = Book
         fields = ["id", "url", "title", "isbn", "pub_date", "authors"]
