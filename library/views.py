bec32bf98b297b5e5c767d74584feb53f9dd24d2
Wondrushh <ondrej.vodicka32@gmail.com>
2024-07-21 19:46:34 +0200

    feat: add SimpleGenericFilter to all views

--- library/views.py
+++ library/views.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.core import exceptions
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.request import Request
 from rest_framework.response import Response
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
+from .filters import SimpleGenericFilter
 
 
 class AuthorViewSet(ModelViewSet):
     queryset = Author.objects.all().order_by("-add_date")
     serializer_class = AuthorSerializer
 
+    def get_queryset(self):
+        queryset = super(AuthorViewSet, self).get_queryset()
+        return self.apply_filters(queryset)
 
-class BookViewSet(ModelViewSet):
+
+class BookViewSet(ModelViewSet, SimpleGenericFilter):
     queryset = Book.objects.all().order_by("-add_date")
     serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = super(BookViewSet, self).get_queryset()
+        return self.apply_filters(queryset)
+        
