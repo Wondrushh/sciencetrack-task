a445dee160e7497ff87d52c4b6e12c9598bf2567
Wondrushh <ondrej.vodicka32@gmail.com>
2024-07-21 19:49:45 +0200

    feat: add documentation to the SimpleGenericFilter

--- library/filters.py
+++ library/filters.py
@@ -2,7 +2,30 @@ from django.core import exceptions
 from django.db.models import QuerySet
 
 class SimpleGenericFilter():
+    """ A simple generic filter mixin for Django Rest Framework ViewSets.
+    This mixin is used to apply filters to the queryset based on query parameters. 
+    """
     def apply_filters(self, default_queryset) -> QuerySet:
+        """ Apply filters to the queryset based on query parameters. 
+        Used in the get_queryset method of a ViewSet.
+        
+        Example:
+        ```
+        class BookViewSet(ModelViewSet, SimpleGenericFilter):
+            queryset = Book.objects.all()
+            serializer_class = BookSerializer
+
+            def get_queryset(self):
+                queryset = super(BookViewSet, self).get_queryset()
+                return self.apply_filters(queryset)
+        ```
+
+        :param default_queryset: The default queryset to apply filters to.
+        :type default_queryset: QuerySet
+        :raises exceptions.BadRequest: If an invalid query parameter is provided.
+        :return: The filtered queryset.
+        :rtype: QuerySet
+        """
         queryset = default_queryset
         for key, value in self.request.query_params.items():
             query_param = { key: value }
