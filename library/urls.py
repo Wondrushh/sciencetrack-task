7af5adc114f8da8d0e2e7a2457b5c000185ae606
Wondrushh <ondrej.vodicka32@gmail.com>
2024-07-22 14:29:40 +0200

    feat: add token obtaining and refreshing endpoints, set view permissions to authorized or read only, add authorization via jwt

--- library/urls.py
+++ library/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-from library import views
+from . import views
 
 router = DefaultRouter()
 router.register("authors", views.AuthorViewSet)
 router.register("books", views.BookViewSet)
 
 urlpatterns = [
-    # path("", views.index, name="index"),
     path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
